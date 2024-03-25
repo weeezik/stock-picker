e22e96e6038945d382ebc22f70f229f15d361b73
weeezik <mkihanya.33@gmail.com>
2024-03-25 18:03:54 +0000

    Add script file to repo
    Some initial thoughts about the problem. Created function skeleton and stock prices array.

---
+++ script.rb
@@ -0,0 +1,15 @@
+
+# find largest stock_value (element) and its day (index) then find smallest stock_value and its day
+  #note, you have to buy before you sell (smaller number should come before higher number)
+# return highest numerical value
+# return index (day) of best time to buy (smallest stock_value), and best time to sell (largest stock_value)
+  #best time to buy has to come before best time to buy, else [run it a different way]
+
+  require 'pry-byebug'
+
+stock_prices = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker (stock_prices)
+  
+end
+
