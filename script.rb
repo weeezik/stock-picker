05180dbb4c61b00fde41dd7f9baedfcc8ccd313f
weeezik <mkihanya.33@gmail.com>
2024-03-25 18:24:58 +0000

    Jot comments with steps to solve
    And a simple use of the .each_with_index function in use to test and confirming I am using it properly.

--- script.rb
+++ script.rb
@@ -5,11 +5,23 @@
 # return index (day) of best time to buy (smallest stock_value), and best time to sell (largest stock_value)
   #best time to buy has to come before best time to buy, else [run it a different way]
 
-  require 'pry-byebug'
-
+require 'pry-byebug'
 stock_prices = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker (stock_prices)
-  
+#correct position confirmation
+  #buy_stock_price has to have smaller index than sell_stock_price
+
+#calculation
+  #buy_stock_price has to have a smaller value than sell_stock_price
+
+  #could be a few different day-pair candidates, calculate the one with the highest profit!
+    #for example [3 and 15] or [1 and 10]
+#index (day) return
+  stock_prices.each_with_index do |stock_value, index|
+    puts "On Day #{index} the stock price is $#{stock_value}."
+  end
 end
+
+puts stock_picker(stock_prices)
 
