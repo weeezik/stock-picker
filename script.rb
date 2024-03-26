fa2b33f77e6dd9351a90807fba40f37054439acf
weeezik <mkihanya.33@gmail.com>
2024-03-26 18:28:08 +0000

    Realized the .minmax method won't work well for this instance.

--- script.rb
+++ script.rb
@@ -1,30 +1,26 @@
-
 # find largest stock_value (element) and its day (index) then find smallest stock_value and its day
-  #note, you have to buy before you sell (smaller number should come before higher number)
+# note, you have to buy before you sell (smaller number should come before higher number)
 # return highest numerical value
 # return index (day) of best time to buy (smallest stock_value), and best time to sell (largest stock_value)
-  #best time to buy has to come before best time to buy, else [run it a different way]
+# best time to buy has to come before best time to buy, else [run it a different way]
 
 require 'pry-byebug'
-stock_prices = [17,3,6,9,15,8,6,1,10]
+stock_prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
 
-def stock_picker (stock_prices)
-buy_stock_price = stock_prices.min
-puts "Buy price is: #{buy_stock_price}"
-sell_stock_price = stock_prices.max
-puts "Sell price is: #{sell_stock_price}"
-
-
-valid_buy_sell_pairs = while stock_prices.index(buy_stock_price) < stock_prices.index(sell_stock_price) do
-  stock_prices.minmax
+def stock_picker(stock_prices)
+  buy_stock_price = stock_prices.min
+  puts "Buy price is: #{buy_stock_price}"
+  sell_stock_price = stock_prices.max
+  puts "Sell price is: #{sell_stock_price}"
+  # binding.pry
+  stock_prices.each_with_index do |_stock_value, _day|
+    # binding.pry
+    while stock_prices.index(buy_stock_price) < stock_prices.index(sell_stock_price)
+      # binding.pry
+      p stock_prices.minmax
+      # binding.pry
+    end
+  end
 end
-
-p valid_buy_sell_pairs
-
-end
-
 
 puts stock_picker(stock_prices)
-
-#methods to use
-  #.minmax
